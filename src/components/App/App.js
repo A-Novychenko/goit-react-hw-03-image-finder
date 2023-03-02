b291b00405b3fa7d50a7c2a3cf0b7bb17c3dd00e
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-03-02 12:20:27 +0200

    add-base-files

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import { GlobalStyle } from 'constants/GlobalStyles';
+
+// import { Container, Wrap, MainTitle, Title, StyleArround } from './App.styled';
+
+export const App = () => {
+  return (
+    <>
+      <GlobalStyle></GlobalStyle>;
+    </>
+  );
+};
